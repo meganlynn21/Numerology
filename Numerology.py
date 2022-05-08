9bcc92c57ec83f35d4d47b0435e39afe05d5ea60
meganlynn21 <meganlkeyes0203@gmail.com>
2022-05-08 01:04:51 -0400

    finished the project and added the power nane number method which calculates the sum of the soul and personality number, then reduces that number

--- Numerology.py
+++ Numerology.py
@@ -1,15 +1,14 @@
 class Numerology:
     # First part of the code to set up and store
-    # and protect/Encapsulate the name and dob :
+    # and protect/Encapsulate the name,dob, conversion_vowels, and conversion_consonants :
     def __init__(self, name, dob):
         self.__name = name
         self.__dob = dob
         self.__conversion_vowels = {"A": 1, "U": 3, "E": 5, "O": 6, "I": 9, " ": 0}
         self.__conversion_consonants = {"S": 1, "J": 1, "B": 2, "K": 2, "T": 2, "C": 3, "D": 4, "M": 4, "V": 4, "W": 5,
-                                 "N": 5, "X": 6, "F": 6, "G": 7, "P": 7, "Y": 7, "H": 8, "Q": 8, "Z": 8, "R": 9, " ": 0}
+                                        "N": 5, "X": 6, "F": 6, "G": 7, "P": 7, "Y": 7, "H": 8, "Q": 8, "Z": 8, "R": 9,
+                                        " ": 0}
 
-
-    # coding the "getters"
     def get_name(self):
         """Returns the subjects name"""
         return self.__name
@@ -18,14 +17,18 @@ class Numerology:
         """Returns the subjects date of birth"""
         return self.__dob
 
+    # Adds the birth month and the birthdate from dob, then takes the sum of that number and reduces it
     def get_attitude(self, dob):
         """Returns the computed attitude number"""
         split_dob = dob.split('-')
         join_dob = ''.join(split_dob)
         day_and_month = join_dob[0:4]
         result = list(map(int, str(day_and_month)))
-        return sum(result)
+        sum_of_list = sum(result)
+        result2 = list(map(int, str(sum_of_list)))
+        return sum(result2)
 
+    # Extracts the day from the dob and adds the 2 numbers together, then reduces the sum of that number
     def get_birthday(self, dob):
         """Returns the computed birthday number"""
         split_dob = dob.split('-')
@@ -36,20 +39,19 @@ class Numerology:
         result2 = list(map(int, str(sum_of_list)))
         return sum(result2)
 
-
+    # Is computed by taking all digits in the dob and adding them together then reducing the sum
     def get_life_path(self, num):
         """Returns the computed life path number"""
-        split_dob = list(num)
-        new_list = []
-        for x in split_dob:
-            new_list.append(x.replace('-', ''))
-        join_list = ''.join(new_list)
-        nums = int(join_list)
+        split_dob = num.split('-')
+        join_dob = ''.join(split_dob)
+        nums = int(join_dob)
         result = list(map(int, str(nums)))
         sum_of_list = sum(result)
         result2 = list(map(int, str(sum_of_list)))
         return sum(result2)
 
+    # Takes all the consonants in name and uses the consonant dictionary to get the number for that letter
+    # then adds those letters together and reduces the sum
     def get_personality(self, name):
         """ Returns the computed personality number"""
         res = 0
@@ -65,12 +67,26 @@ class Numerology:
         nums = int(join_list)
         result = list(map(int, str(nums)))
         sum_of_list = sum(result)
+        result2 = list(map(int, str(sum_of_list)))
+        return sum(result2)
+
+    # Is computed by adding the soul and personality number together then reducing that number
+    def get_power_name(self, soul, personality):
+        """Returns the computed power name number"""
+        total = soul + personality
+        str_res = str(total)
+        split_name = list(str_res)
+        new_list = []
+        for x in split_name:
+            new_list.append(x)
+        join_list = ''.join(new_list)
+        nums = int(join_list)
+        result = list(map(int, str(nums)))
+        sum_of_list = sum(result)
         return sum_of_list
 
-    def get_power_name(self):
-        """Returns the computed power name number"""
-        return self.__power_name
-
+    # Takes all the vowels in name and uses the vowel dictionary to get the number for that letter
+    # then adds those letters together and reduces the sum
     def get_soul(self, name):
         """ returns the computed soul number"""
         res = 0
--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 
-
 import datetime
 import Numerology
 
 
 def main():
-    is_valid = True
-    while is_valid:
+
+    while True:
         try:
-            name = input("whats your full name? ").upper()
+            name = input("Whats your full name? ").upper()
             if not name:
-                raise ValueError('empty string')
+                raise ValueError('Empty string')
             break
         except ValueError:
             main()
-    while is_valid:
-        dob = input("whats your date of birth, MM-DD-YYYY format ")
+    while True:
+        dob = input("Whats your date of birth, MM-DD-YYYY format ")
         try:
             datetime.datetime.strptime(dob, "%m-%d-%Y")
             break
         except ValueError:
-            print("please input correct date format")
+            print("Please input correct date format")
 
     your_numerology = Numerology.Numerology(name, dob)
     life_path = your_numerology.get_life_path(dob)
+    soul_num = your_numerology.get_soul(name)
+    personality_num = your_numerology.get_personality(name)
     print(f"Test Name: {your_numerology.get_name()}")
     print(f"Test DOB: {your_numerology.get_dob()}")
     print(f"Life Path Number: {life_path}")
     print(f"Birth Day Number: {your_numerology.get_birthday(dob)}")
     print(f"Attitude Number: {your_numerology.get_attitude(dob)}")
-    print(f"Soul Number: {your_numerology.get_soul(name)}")
-    print(f"Personality Number: {your_numerology.get_personality(name)}")
+    print(f"Soul Number: {soul_num}")
+    print(f"Personality Number: {personality_num}")
+    print(f"Power Name Number: {your_numerology.get_power_name(soul_num, personality_num)}")
+    should_continue = input("Do you want to play again? Put y for yes or n for no: ").lower()
+    if should_continue == "y":
+        main()
+    else:
+        exit()
+
 
 main()
 
